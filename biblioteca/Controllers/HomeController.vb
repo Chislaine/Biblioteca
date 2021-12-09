5224872d06d94343cc19fd3ed342ae0abf8f2206
Chislaine <87792085+Chislaine@users.noreply.github.com>
2021-12-09 14:02:49 -0300

    Add files via upload

---
+++ biblioteca/Controllers/HomeController.vb
@@ -0,0 +1,20 @@
+﻿Public Class HomeController
+    Inherits System.Web.Mvc.Controller
+
+    Function Index() As ActionResult
+        Return View()
+    End Function
+
+    Function Sobre() As ActionResult
+        ViewData("Message") = "Somos uma rede de bibliotecas espalhadas pelo mundo, procurando trazer até o conforto da sua casa, tudo o que um livro lhe porporciona em bem estar, aventuras, suspenses, romances e tudo mais que o mundo da magia lhe permitir ver e sentir."
+
+        Return View()
+    End Function
+
+    Function Contato() As ActionResult
+        ViewData("Message") = "Para nos contatar é bem simples, é só discar 323265987 ou 56487941."
+
+        Return View()
+    End Function
+End Class
+
